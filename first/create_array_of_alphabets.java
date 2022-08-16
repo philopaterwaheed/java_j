e008007df1aa0486702be8de7d1a29d2d029cfee
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2022-08-16 23:49:40 +0000

    Create create_array_of_alphabets.java

---
+++ first/create_array_of_alphabets.java
@@ -0,0 +1,13 @@
+ public  static boolean check(String sentence){
+        char [] letters = new char [26] ;  
+        for (int a = 0;  a <= 25  ; a++)
+           { letters[a]= (char) (a+49+48);
+             System.out.print(letters[a]);
+           }
+    
+
+            
+        return true ; 
+      }
+
+}
